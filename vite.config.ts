921bdd95860cde39e07d31b6480e2fdc49599fd9
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2023-11-05 18:43:28 +0100

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-fishing-app/',
+  base: process.env.NODE_ENV === 'production' ? 'damienld22.github.io/my-fishing-app/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
